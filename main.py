73b3ced28a4dd0e8e8b09473d4c74eb65c97397c
son.nguyen <son.nguyen@xaiondata.co.jp>
2025-03-22 17:06:25 +0900

    chore: add gitignore and try to run ruff format

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from portfolio-management!")
+from typing import Iterable
 
 
-if __name__ == "__main__":
-    main()
+def sum_even_numbers(numbers: Iterable[int]) -> int:
+    """Given an iterable of integers, return the sum of all even numbers in the iterable."""
+    return sum(num for num in numbers if num % 2 == 0)
